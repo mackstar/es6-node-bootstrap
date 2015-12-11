80bd9b7ddf4334c3afbea919513ad568361d1140
Richard McIntyre <richard.mackstar@gmail.com>
2015-12-11 13:39:17 +0000

    Added initial node files.

---
+++ bin/start.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+'use strict';
+
+require('babel/register');
+
+var path = require('path');
+
+require('app-module-path').addPath(path.join(__dirname, '../lib'));
+
+var env = process.env.NODE_ENV || 'development';
+
+var Start = require('start');
+new Start();
